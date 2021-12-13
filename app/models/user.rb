ff6fda7e07cf7db29ad9575d9f5fe7ea6a03fcb6
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-12-13 13:11:35 +0100

    Feat: added user validations and user instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+  validates :first_name ,presence: true
+  validates :last_name ,presence: true
+  validates :role ,presence: true
+  validates :team_role ,presence: true
+  ROLES = ["admin", "regular"]
+  TEAM_ROLES = ["commander", "astronaut"]
+
+  def astronaut?
+    self.role == "astronaut"
+  end
+
+  def commander?
+    self.role == "commander"
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
